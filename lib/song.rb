d6c893ba59a9303d5c07a46bab3eb975cc9f2bbf
lieby weintraub <liebahinda@gmail.com>
2019-03-12 03:20:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,47 @@
-class Song 
-  @@count=0
-  attr_accessor :name, :artis, :genre
-  def initialize(name, artist, genre)
-    @name=name
-    @artist=artist
-    @genre=genre 
-    @@count +=1 
-  end 
-  def self.count
-    @@count
-  end 
+class Song
+  @@count = 0
+    @@genres = []
+    @@artists = []
+    def self.count
+      @@count 
+    end
+    def self.genres
+       @@genres.uniq
+    end
+    def self.artists
+      @@artists.uniq
+    end
+
+   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+
+   def self.artist_count
+    artist_count = {}
+    @@artists.each do |song|
+      if artist_count[song]
+        artist_count[song] += 1 
+      else
+        artist_count[song] = 1
+      end
+    end
+    artist_count
+  end
 end 
